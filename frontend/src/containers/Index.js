6972cb7be2e2609667b6faf51a9feab5b5c928d9
cristinapana <cristina@dovelopers.com>
2020-04-26 09:29:37 +0300

    added text on homepage

--- frontend/src/containers/Index.js
+++ frontend/src/containers/Index.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class Index extends Component {
     render() {
-      return (<h1>WebTech Superheroes Code</h1>)
+      return (
+        <div>
+          <h1>Welcome to WebTech Superheroes Code</h1> 
+          <div>
+              <p>Before you take a peek in the platform, please see a quick guide on how to make the most out of it.</p>
+              <p>First thing first, make sure your profile has your Git profile link.</p>
+              <p>  If you don’t have an account yet on Git Hub, click here.</p>
+          </div>
+        </div>)
     }
 }
 
