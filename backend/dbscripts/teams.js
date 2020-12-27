cba8efe5624ffce7ffb3201c8b524f03aaf11e58
Eduard Budacu <eduard.budacu@gmail.com>
2020-12-27 09:20:14 +0000

    assignments and teams update

--- backend/dbscripts/teams.js
+++ backend/dbscripts/teams.js
@@ -1,16 +1,21 @@
 var db = require('../models/index');
 
-var teams = require("../sample-data/teams_2020.json")
+var teams = require("../sample-data/teams_2020.json");
+
+(async () => {
+
+await db.Teams.destroy({ where: {
+    id: {
+        [db.Sequelize.Op.gt]: 1
+    }
+}})
 
 teams.forEach(async (team) => {
     try {
-        await db.Teams.destroy({ where: {
-            id: {
-                [db.Sequelize.Op.gt]: 1
-            }
-        }})
         await db.Teams.create(team)
     } catch(err) {
         console.log(err)
     }
 })
+
+})()
--- frontend/src/components/teams/Team.js
+++ frontend/src/components/teams/Team.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import Button from '@material-ui/core/Button';
 import ListItemText from '@material-ui/core/ListItemText';
-
+import Box from '@material-ui/core/Box';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
 class Team extends Component {
@@ -11,16 +11,18 @@ class Team extends Component {
       let team = this.props.team;
       
       return (
-        <ListItem>
-            <ListItemText>{team.name}</ListItemText>
-            <Button
-              href={team.projectRepo}
-              target="_blank"
-              variant="contained" color="primary"
-              startIcon={<GitHubIcon />}
-            >Open Repo</Button>
-            
-      </ListItem>
+        <Box borderBottom={1}>
+          <ListItem >
+              <ListItemText>{team.name}</ListItemText>
+              <Button
+                href={team.projectRepo}
+                target="_blank"
+                variant="contained" color="primary"
+                startIcon={<GitHubIcon />}
+              >Open Repo</Button>
+        </ListItem>
+      </Box>
+        
       )  
     }
   }
